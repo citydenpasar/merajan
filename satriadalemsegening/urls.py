df9d1daa6ff709a5cafb84f752d7aefc3b482e67
Permadi Ajus <permadiajus@gmail.com>
2023-04-21 15:40:36 +0800

    Perubahan Kedua

--- satriadalemsegening/urls.py
+++ satriadalemsegening/urls.py
@@ -19,5 +19,5 @@ from django.urls import path
 from myapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name="index"),
+    path('', views.index, name="halaman_awal"),
 ]
