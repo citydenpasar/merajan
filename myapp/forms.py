c4a2f4ca0b2731f3923850756256b92f40fea471
Permadi Ajus <permadiajus@gmail.com>
2023-04-24 14:38:09 +0800

    Memperbaiki Halaman Komentar

--- myapp/forms.py
+++ myapp/forms.py
@@ -4,4 +4,9 @@ from .models import Komentar
 class KomentarForm(forms.ModelForm):
     class Meta:
         model = Komentar
-        fields = ['nama', 'isi_komentar']
+        fields = ['nama', 'isi_komentar', 'parent']
+        widgets = {
+            'nama': forms.TextInput(attrs={'class': 'form-control'}),
+            'isi_komentar': forms.Textarea(attrs={'class': 'form-control'}),
+            'parent': forms.HiddenInput(),
+        }
